563a1761210076d9fb37a4ab64c9a6135f56c2b7
yahyokhoja <yahuokhoja@gmail.com>
2025-05-23 18:58:45 +0500

    кнопка войти

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/', profile_view, name='profile'),  # если есть
     
